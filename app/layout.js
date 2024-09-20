8190d607013b76e7a198a017e616eed35d8601fd
Iba Yuto <YOUR_EMAIL>
2024-09-21 02:22:03 +0900

    .

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 // -memo, Speed Insights使ってみる ( Vercel Speed Insights provides you detailed insights into your website's performance )
-// versel mypageのspeed insightsという項目から確認できるようになる
+// versel mypageのspeed insightsという項目から確認できるようになる.
 // https://vercel.com/docs/speed-insights/quickstart#add-the-speedinsights-component-to-your-app
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
