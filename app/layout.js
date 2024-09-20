85e5a0a8fe3ee4d0faead021014e63a63764ebe7
Iba Yuto <iba_yuto@giginc.co.jp>
2024-09-21 02:09:27 +0900

    add: Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,8 @@
 import "./globals.css";
+// -memo, Speed Insights使ってみる ( Vercel Speed Insights provides you detailed insights into your website's performance )
+// versel mypageのspeed insightsという項目から確認できるようになる
+// https://vercel.com/docs/speed-insights/quickstart#add-the-speedinsights-component-to-your-app
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "study next.js",
@@ -9,7 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ja">
       {/* -memo, JSX特有のclassNameという書き方でクラス属性のプロパティを指定する必要がある */}
-      <body className="container">{children}</body>
+      <body className="container">
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
